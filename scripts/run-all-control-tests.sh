076b24d3f8a6d03a9f84e4ed039eefe786f7591a
Ben <ben@armosec.io>
2023-01-08 20:51:28 +0200

    installing python from action

--- scripts/run-all-control-tests.sh
+++ scripts/run-all-control-tests.sh
@@ -12,13 +12,26 @@ fi
 # Print Kubernetes version
 kubectl version
 
+# Check if python or python3 is available
+PYTHON_EXECUTABLE=na
+if command -v python3 &> /dev/null
+then
+    PYTHON_EXECUTABLE=python3
+elif command -v python &> /dev/null
+then
+    PYTHON_EXECUTABLE=python
+else
+    echo "python could not be found"
+    exit 1
+fi
+
 # Result variable
 result=0
 # Run all control tests
 for control in $(ls controls); do
     echo "Running test $control"
     pushd controls/$control
-    python3 ../../scripts/run-control-test.py
+    $PYTHON_EXECUTABLE ../../scripts/run-control-test.py
     # Check if test failed
     if [ $? -ne 0 ]; then
         echo "Test $control failed"
