bf00bd03f528c5e9c833d3e20e4083332d4c12fa
Ben <ben@armosec.io>
2023-01-11 21:27:34 +0200

    Fixed some minor issues in the create release yaml script

--- scripts/create-release-objects.sh
+++ scripts/create-release-objects.sh
@@ -14,13 +14,14 @@ RELEASE_DIR=$1
 RELEASE_POLICY_YAML_FILE_NAME=$RELEASE_DIR/kubescape-validating-admission-policies.yaml
 
 echo "Creating release policy YAML file $RELEASE_POLICY_YAML_FILE_NAME"
-echo "" > $RELEASE_POLICY_YAML_FILE_NAME
+touch $RELEASE_POLICY_YAML_FILE_NAME
 
 # Loop through all policies and add them to the release YAML file
 for control in $(ls controls); do
     # Verify if policy file is in the control directory exists
     if [ ! -f "controls/$control/policy.yaml" ]; then
         echo "Warning: Policy file for control $control does not exist"
+        continue
     fi
     # Copy policy file contents to release YAML file
     cat controls/$control/policy.yaml >> $RELEASE_POLICY_YAML_FILE_NAME
@@ -28,8 +29,6 @@ for control in $(ls controls); do
 done
 
 # Delete the last line of the policy file
-cp $RELEASE_POLICY_YAML_FILE_NAME $RELEASE_POLICY_YAML_FILE_NAME.bak
-tail -n +2 $RELEASE_POLICY_YAML_FILE_NAME.bak > $RELEASE_POLICY_YAML_FILE_NAME
-rm $RELEASE_POLICY_YAML_FILE_NAME.bak
+head -n -3 $RELEASE_POLICY_YAML_FILE_NAME > tmp.txt && mv tmp.txt $RELEASE_POLICY_YAML_FILE_NAME
 
 exit 0
