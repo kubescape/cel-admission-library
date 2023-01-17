e40355b1cfda7e7278fcf00a2b88bd054b611c21
Ben <ben@armosec.io>
2023-01-17 11:53:21 +0200

    argument parsing added

--- tools/celcli/celcli.go
+++ tools/celcli/celcli.go
@@ -2,20 +2,30 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"log"
 	"os"
 
 	"github.com/google/cel-go/cel"
+	"gopkg.in/yaml.v3"
 )
 
 func main() {
+	// Parse the command line arguments.
+	// Add input format flag
+	var inputFormat string
+	flag.StringVar(&inputFormat, "input", "json", "Input format (json, yaml, etc.)")
+	flag.Parse()
 	// Check that we have argument to evaluate.
-	if len(os.Args) < 2 {
+	if flag.NArg() != 1 {
 		log.Fatalf("usage: %s <expression>", os.Args[0])
 	}
+	if inputFormat != "json" && inputFormat != "yaml" {
+		log.Fatalf("input format %s not supported", inputFormat)
+	}
 	// Take the first argument as the expression to evaluate.
-	cel_expr := os.Args[1]
+	cel_expr := flag.Arg(0)
 
 	// Create the environment and parse the expression.
 	env, err := cel.NewEnv(
@@ -34,8 +44,14 @@ func main() {
 	}
 	// Read the object from the STDIN and evaluate the expression as a JSON object.
 	var data map[string]interface{}
-	if err := json.NewDecoder(os.Stdin).Decode(&data); err != nil {
-		log.Fatalf("error reading json: %s", err)
+	if inputFormat == "yaml" {
+		if err := yaml.NewDecoder(os.Stdin).Decode(&data); err != nil {
+			log.Fatalf("error reading yaml: %s", err)
+		}
+	} else if inputFormat == "json" {
+		if err := json.NewDecoder(os.Stdin).Decode(&data); err != nil {
+			log.Fatalf("error reading json: %s", err)
+		}
 	}
 
 	out, _, err := prg.Eval(map[string]interface{}{
